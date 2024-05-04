04edd2367faa081b72c530949e0d0ab28949d9f0
mary <aksenovamaga@rambler.ru>
2024-05-04 19:56:40 +0300

    Lab 10

--- backend/front/src/services/BackendService.jsx
+++ backend/front/src/services/BackendService.jsx
@@ -2,8 +2,43 @@
 import axios from 'axios'
 import Utils from "../utils/Utils";
 
+import {alertActions, store} from "../utils/Rdx";
+
+
 const API_URL = 'http://localhost:8081/api/v1'
 const AUTH_URL = 'http://localhost:8081/auth'
+
+
+function showError(msg)
+{
+    store.dispatch(alertActions.error(msg))
+}
+
+axios.interceptors.request.use(
+    config => {
+        store.dispatch(alertActions.clear())
+        let token = Utils.getToken();
+        if (token)
+            config.headers.Authorization = token;
+        return config;
+    },
+    error => {
+        showError(error.message)
+        return Promise.reject(error);
+    })
+
+axios.interceptors.response.use(undefined,
+    error => {
+        if (error.response && error.response.status && [401, 403].indexOf(error.response.status) !== -1)
+            showError("Ошибка авторизации")
+        else if (error.response && error.response.data && error.response.data.message)
+            showError(error.response.data.message)
+        else
+            showError(error.message)
+        return Promise.reject(error);
+    })
+
+
 
 class BackendService {
 
@@ -12,8 +47,9 @@ class BackendService {
     }
 
     logout() {
-            return axios.get(`${AUTH_URL}/logout`, { headers : {Authorization : Utils.getToken()}})
+            return axios.get(`${AUTH_URL}/logout`)
     }
+
 }
 
 export default new BackendService()
